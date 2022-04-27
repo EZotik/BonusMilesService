e510c4482a36ad5128483644d49b184e0e900f8d
karmengerl <karmengerl@gmail.com>
2022-04-27 22:56:14 +0300

    Decision 1.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 100;
+        int numberRubles = 20;
+        int mileBonus = ticketPrice / numberRubles;
+        System.out.println(mileBonus);
+    }
+}
